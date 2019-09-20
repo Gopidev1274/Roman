8e2ce5180dd186f5f6d1945c3965609b2fccea24
Gopidev1274 <gopigamer.1274@gmail.com>
2019-09-20 16:43:51 +0530

    overload code

---
+++ src/org/rr/Reigns.java
@@ -0,0 +1,11 @@
+package org.rr;
+
+public class Reigns {
+public void rr(int id) {
+	System.out.println("Enter the value: "+id);
+}
+public static void main(String[] args) {
+	Reigns a=new Reigns();
+	a.rr(10);
+}
+}
